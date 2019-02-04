71b73f6674680e328aeab291fd46b4dabac750bd
David Lenz <captainkrypto@gmail.com>
2019-02-04 16:31:18 -0500

    Initial commit

---
+++ homeassistant/python_scripts/get_south_beach_latest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import urllib3
+import json
+import requests
+import os
+
+http = urllib3.PoolManager()
+
+r = http.request('GET', 'http://video-monitoring.com/beachcams/boca/latest.json')
+latest = json.loads(r.data.decode('utf-8'))
+base_url = 'http://video-monitoring.com/beachcams/boca/'
+pavillion_url = latest['s4']['hr']
+complete_pavillion_url = base_url + pavillion_url
+beach_url = latest['s8']['hr']
+complete_beach_url = base_url + beach_url
+
+with open('../www/images/south_beach_latest_pavillion.jpg', 'wb') as handle:
+        response = requests.get(complete_pavillion_url, stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
+
+with open('../www/images/south_beach_latest_beach.jpg', 'wb') as handle:
+        response = requests.get(complete_beach_url, stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
